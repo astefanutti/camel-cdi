6719200334a67bc7596363a1c20712944c30b768
Charles Moulliard <ch007m@gmail.com>
2014-11-27 18:47:18 +0100

    Replace license with Apache License

--- envs/se/src/main/java/org/apache/camel/cdi/se/bean/SimpleCamelRoute.java
+++ envs/se/src/main/java/org/apache/camel/cdi/se/bean/SimpleCamelRoute.java
@@ -1,11 +1,12 @@
 /**
- * Copyright (C) 2014 Antonin Stefanutti (antonin.stefanutti@gmail.com)
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +17,6 @@
 package org.apache.camel.cdi.se.bean;
 
 import org.apache.camel.Endpoint;
-import org.apache.camel.EndpointInject;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.cdi.Uri;
 import org.apache.camel.component.mock.MockEndpoint;
