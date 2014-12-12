f1cf92904ff791ee1a1dac67dc0530e45452ae1c
Charles Moulliard <ch007m@gmail.com>
2014-12-12 22:37:59 +0100

    Revert "Fixing Jetty example and adding code to see it run"

--- quickstarts/simplecontextname/src/main/java/org/apache/camel/cdi/example1/HelloBean.java
+++ quickstarts/simplecontextname/src/main/java/org/apache/camel/cdi/example1/HelloBean.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.cdi.jettyexample;
+package org.apache.camel.cdi.example1;
 
 import org.apache.camel.Body;
 
