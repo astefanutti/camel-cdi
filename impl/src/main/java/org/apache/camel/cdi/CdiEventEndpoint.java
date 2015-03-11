0c0694767e6a6ba0ea80fb01e17d7f35e85b25f5
astefanutti <antonin.stefanutti@murex.com>
2015-03-11 10:37:10 +0100

    Add Javadoc to the CDI event endpoint

--- impl/src/main/java/org/apache/camel/cdi/CdiEventEndpoint.java
+++ impl/src/main/java/org/apache/camel/cdi/CdiEventEndpoint.java
@@ -18,6 +18,7 @@ package org.apache.camel.cdi;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Consumer;
+import org.apache.camel.Endpoint;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.impl.DefaultEndpoint;
@@ -29,10 +30,60 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * A Camel {@link Endpoint} that bridges the CDI events facility with Camel routes so that CDI events
+ * can be seamlessly observed / consumed (respectively produced / fired) from Camel consumers (respectively by Camel producers).
+ * <p/>
+ * The {@code CdiEventEndpoint<T>} bean can be used to observe / consume CDI events whose event type is {@code T}, for example:
+ * <pre><code>
+ *     {@literal@}Inject
+ *     CdiEventEndpoint{@literal<}String{@literal>} cdiEventEndpoint;
+ *
+ *     from(cdiEventEndpoint).log("CDI event received: ${body}");
+ * </code></pre>
+ *
+ * Conversely, the {@code CdiEventEndpoint<T>} bean can be used to produce / fire CDI events whose event type is {@code T}, for example:
+ * <pre><code>
+ *     {@literal@}Inject
+ *     CdiEventEndpoint{@literal<}String{@literal>} cdiEventEndpoint;
+ *
+ *     from("direct:event").to(cdiEventEndpoint).log("CDI event sent: ${body}");
+ * </code></pre>
+ *
+ * The type variable {@code T}, respectively the qualifiers, of a particular {@code CdiEventEndpoint<T>} injection point
+ * are automatically translated into the parameterized <i>event type</i>, respectively into the <i>event qualifiers</i>, e.g.:
+ *  <pre><code>
+ *      {@literal@}Inject
+ *      {@literal@}FooQualifier
+ *      CdiEventEndpoint{@literal<}List{@literal<}String{@literal>}{@literal>} cdiEventEndpoint;
+ *
+ *      from("direct:event").to(cdiEventEndpoint);
+ *
+ *      void observeCdiEvents({@literal@}Observes {@literal@}FooQualifier List{@literal<}String{@literal>} event) {
+ *          logger.info("CDI event: {}", event);
+ *      }
+ *  </code></pre>
+ *
+ *  When multiple Camel contexts exist in the CDI container, the {@code @ContextName} qualifier can be used
+ *  to qualify the {@code CdiEventEndpoint<T>} injection points, e.g.:
+ *  <pre><code>
+ *     {@literal@}Inject
+ *     {@literal@}ContextName("foo")
+ *     CdiEventEndpoint{@literal<}List{@literal<}String{@literal>}{@literal>} cdiEventEndpoint;
+ *     // Only observe / consume events having the {@literal@}ContextName("foo") qualifier
+ *     from(cdiEventEndpoint).log("Camel context 'foo' > CDI event received: ${body}");
+ *     // Produce / fire events with the {@literal@}ContextName("foo") qualifier
+ *     from("...").to(cdiEventEndpoint);
+ *
+ *     void observeCdiEvents({@literal@}Observes {@literal@}ContextName("foo") List{@literal<}String{@literal>} event) {
+ *         logger.info("Camel context 'foo' > CDI event: {}", event);
+ *     }
+ * </code></pre>
+ */
 public final class CdiEventEndpoint<T> extends DefaultEndpoint {
 
     private final List<CdiEventConsumer<T>> consumers = new ArrayList<>();
-    
+
     private final Event<T> event;
 
     CdiEventEndpoint(Event<T> event, String endpointUri, CamelContext context, ForwardingObserverMethod<T> observer) {
