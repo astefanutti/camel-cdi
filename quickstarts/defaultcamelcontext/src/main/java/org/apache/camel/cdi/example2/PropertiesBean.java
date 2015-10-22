1b926437bb3c3e4ac19af6a17491eaf2c8c5e4d8
Antonin Stefanutti <antonin@stefanutti.fr>
2015-10-22 15:24:52 +0200

    Upgrade DeltaSpike to version 1.5.0

--- quickstarts/defaultcamelcontext/src/main/java/org/apache/camel/cdi/example2/PropertiesBean.java
+++ quickstarts/defaultcamelcontext/src/main/java/org/apache/camel/cdi/example2/PropertiesBean.java
@@ -23,7 +23,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 
-public class PropertiesUtil {
+public class PropertiesBean {
 
     @Produces
     @ApplicationScoped
