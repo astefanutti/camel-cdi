3251b82312130db4b5f0ba5dfe25abdf92d4a771
Antonin Stefanutti <antonin@stefanutti.fr>
2015-11-24 12:12:17 +0100

    Rename OSGi environment test module

--- envs/osgi/src/test/java/org/apache/camel/cdi/osgi/CdiPaxExamKarafTest.java
+++ envs/osgi/src/test/java/org/apache/camel/cdi/osgi/CdiPaxExamKarafTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.cdi.itest;
+package org.apache.camel.cdi.osgi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
