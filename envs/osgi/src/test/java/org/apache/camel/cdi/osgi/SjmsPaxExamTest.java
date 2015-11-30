415c9949acfdc814840b6cd0bb706671be63e0a8
Antonin Stefanutti <antonin@stefanutti.fr>
2015-11-30 18:14:48 +0100

    OSGi test for the SJMS sample

---
+++ envs/osgi/src/test/java/org/apache/camel/cdi/osgi/SjmsPaxExamTest.java
@@ -0,0 +1,89 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.cdi.osgi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.ConfigurationManager;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.options.LogLevelOption;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureConsole;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
+
+@RunWith(PaxExam.class)
+public class SjmsPaxExamTest {
+
+    @Configuration
+    public Option[] config() {
+        String karafVersion = new ConfigurationManager().getProperty("pax.exam.karaf.version", "3.0.2");
+        return new Option[]{
+            karafDistributionConfiguration()
+                .frameworkUrl(
+                    maven("org.apache.karaf", "apache-karaf", karafVersion).type("zip"))
+                .karafVersion(karafVersion)
+                .name("Apache Karaf")
+                .useDeployFolder(false)
+                .unpackDirectory(new File("target/paxexam/unpack/")),
+            keepRuntimeFolder(),
+            // Don't bother with local console output as it just ends up cluttering the logs
+            configureConsole().ignoreLocalConsole(),
+            // Force the log level to INFO so we have more details during the test. It defaults to WARN.
+            logLevel(LogLevelOption.LogLevel.INFO),
+
+            // Option to be used to do remote debugging
+            //debugConfiguration("5005", true),
+
+            // Load Features PAX CDI Weld and Camel CDI
+            features(
+                maven().groupId("org.ops4j.pax.cdi").artifactId("pax-cdi-features").type("xml").classifier("features").versionAsInProject(),
+                "pax-cdi-weld"
+            ),
+            features(
+                maven().groupId("io.astefanutti.camel.cdi").artifactId("camel-cdi").type("xml").classifier("features").versionAsInProject(),
+                "camel-cdi"
+            ),
+            features(
+                maven().groupId("org.apache.karaf.features").artifactId("enterprise").type("xml").classifier("features").versionAsInProject(), "jms"
+            ),
+            features(
+                maven().groupId("org.apache.activemq").artifactId("activemq-karaf").type("xml").classifier("features").versionAsInProject(), "activemq-broker"
+            ),
+            mavenBundle()
+                .groupId("org.apache.camel").artifactId("camel-sjms").versionAsInProject(),
+            mavenBundle()
+                .groupId("commons-pool").artifactId("commons-pool").versionAsInProject(),
+            mavenBundle()
+                .groupId("io.astefanutti.camel.cdi").artifactId("camel-cdi-sample-sjms").versionAsInProject()
+        };
+    }
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
