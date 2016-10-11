7bedc338d8a0d0c73a2cecb6e4b6bc78daf47f0d
Antonin Stefanutti <antonin@stefanutti.fr>
2016-10-11 17:52:12 +0200

    Fix beans.xml in FluentProducerTemplateTest

--- envs/se/src/test/java/org/apache/camel/cdi/se/FluentProducerTemplateTest.java
+++ envs/se/src/test/java/org/apache/camel/cdi/se/FluentProducerTemplateTest.java
@@ -25,6 +25,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -45,7 +46,7 @@ public class FluentProducerTemplateTest {
             // Test class
             .addClass(UriEndpointRoute.class)
             // Bean archive deployment descriptor
-            .addAsManifestResource("beans.xml", "beans.xml");
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
     @Inject
